41340b24c3f81a64f40914ede0da64bb7eac742b
LIJOMLOYID <lijomloyid@gmail.com>
2020-07-22 14:32:07 +0530

    Initial commit

---
+++ movie-catalog-service/src/test/java/com/thinkpalm/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkpalm.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
