92ac002378d4cf24732600355d513ed604ede805
LIJOMLOYID <lijomloyid@gmail.com>
2021-02-11 19:25:12 +0530

    Used Bean to create RestTemplate instance

--- movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,13 @@ import com.thinkpalm.moviecatalogservice.models.Rating;
 @RequestMapping("catalog")
 public class MovieCatalogResource {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String id){
 		
-		RestTemplate restTemplate = new RestTemplate();
+		//RestTemplate restTemplate = new RestTemplate();
 		
 		// get all rated movie IDs
 		
