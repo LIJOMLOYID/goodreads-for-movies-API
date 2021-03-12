2a6874f7325bb7f0468da3011ef911a76c0b0b05
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-12 14:10:12 +0530

    Made call to the Ratings API

--- ratings-data-service/src/main/java/com/thinkpalm/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/thinkpalm/ratingsdataservice/resources/RatingsResource.java
@@ -1,5 +1,8 @@
 package com.thinkpalm.ratingsdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +12,18 @@ import com.thinkpalm.ratingsdataservice.models.Rating;
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
+	
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3)
+		);
+		return ratings;
 	}
 }
