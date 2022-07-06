3286e155f1374ab4b80561626525bb16c6198ec9
LIJOMLOYID <lijomloyid@gmail.com>
2022-07-06 20:02:38 +0530

    feat: discover movie info service and ratings data service through eureka

--- movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/resources/MovieCatalogResource.java
@@ -37,12 +37,12 @@ public class MovieCatalogResource {
 		
 		//WebClient.Builder builder = WebClient.builder();
 		
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"
 				+ userId, UserRating.class);
 		
 		return ratings.getUserRating().stream().map(rating-> {
 			// For each movie ID, call movie info service and get details
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"
 				+ rating.getMovieId(), Movie.class);			
 			// Put them all together
 			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
