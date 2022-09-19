b509e4d330e8d72762ce167bef35bfe08bd6b5bd
LIJOMLOYID <lijomloyid@gmail.com>
2022-09-19 17:43:45 +0530

    feat: add 3 second timeout to API calls from catalog service

--- movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -12,20 +13,22 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 @SpringBootApplication
 @EnableEurekaClient
 public class MovieCatalogServiceApplication {
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+	
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 	
 	@Bean
 	public WebClient.Builder getWebClientBuilder(){
 		return WebClient.builder();
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
 
 }
