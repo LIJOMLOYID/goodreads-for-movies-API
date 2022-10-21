b432730ec9c3c43bf859e7038680a58f53578c15
LIJOMLOYID <lijomloyid@gmail.com>
2022-10-21 16:15:13 +0530

    feat: configure Hystrix parameters using commandProperties @HystrixProperty

--- movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/services/UserRatingInfo.java
+++ movie-catalog-service/src/main/java/com/thinkpalm/moviecatalogservice/services/UserRatingInfo.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 import com.thinkpalm.moviecatalogservice.models.Rating;
 import com.thinkpalm.moviecatalogservice.models.UserRating;
 
@@ -22,7 +24,15 @@ public class UserRatingInfo {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod="getFallbackUserRating")
+	@HystrixCommand(fallbackMethod="getFallbackUserRating", 
+			commandProperties = {
+					@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value= "2000"),
+					@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+					@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+					@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+					
+			}
+	)
 	public UserRating getUserRating(@PathVariable("userId") String userId) {
 		return restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/"
 				+ userId, UserRating.class);
